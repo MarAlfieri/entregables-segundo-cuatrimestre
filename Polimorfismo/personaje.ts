975b53b41c1a44a414a94af8d3d63992d74a9c92
MarAlfieri <alfierisistemas@gmail.com>
2024-10-16 08:53:03 -0300

    aqui vamos otra vez

---
+++ Polimorfismo/personaje.ts
@@ -0,0 +1,30 @@
+// Clase base Personaje
+export abstract class Personaje {
+    protected nombre: string;
+    private nivel: number = 1;
+    private puntosDeVida: number = 100;
+
+    public constructor(nombre: string) {
+        this.nombre = nombre;
+    }
+
+    abstract atacar(): void;
+    abstract defender(): void;
+    abstract nuevoAtaque(): void; // Método abstracto para nuevo ataque
+
+    //Metodos get de propiedades privadas
+    public getNivel(): number {
+        return this.nivel;
+    }
+    public getPuntosDeVida(): number {
+        return this.puntosDeVida;
+    }
+    
+    //Metodos set para modificar propiedades privadas
+    public setNivel(nivel: number): void {
+        this.nivel = nivel;
+    }
+    public setPuntosDeVida(puntosDeVida: number): void{
+        this.puntosDeVida = puntosDeVida;
+    }
+}
