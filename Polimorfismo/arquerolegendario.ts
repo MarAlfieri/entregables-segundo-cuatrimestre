65daed16d33b8d5ff678a2e75f82eab797651393
MarAlfieri <alfierisistemas@gmail.com>
2024-10-18 19:28:55 -0300

    Actualizaciones con arquero legendario

---
+++ Polimorfismo/arquerolegendario.ts
@@ -0,0 +1,23 @@
+import { Arquero } from "./arquero";
+
+export class ArqueroLegendario extends Arquero {
+    private tipoFlecha: string;
+
+    public constructor(nombre: string) {
+        super(nombre);
+    }
+    
+    //Metodo get
+    public getTipoFlecha(): string {
+        return this.tipoFlecha;
+    }
+
+    // Metodos set
+    public disparaFlechaEspecial() {
+        console.log(`${this.nombre} está disparando una flecha ${this.tipoFlecha}`);
+    }
+
+    public setTipoFlecha(tipoFlecha : string): void {
+        this.tipoFlecha = tipoFlecha;
+    }
+}
